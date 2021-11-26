a152afa036e23b5493097e563abd240e65c991e0
Mint Bao <minhbao1223@gmail.com>
2021-11-26 12:02:06 +0700

    oke

--- bai1.cpp
+++ bai1.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-int main() {
-    int n,sum=0;
-    cin>>n;
-    cout <<sum<<endl;
-    for (int i=1 ; i<=n; i +=2){
-        sum =sum +i;
+int nhapso(){
+    int a,b,c;
+    cout << "Nhap lan luot  3 so: ";
+    cin >> a>>b>> c;
+}
+int sapxep(){
+    int a[10];
+    for (int i=1 ; i<=3 ; i++){
+        cin >> a[i];
     }
-    cout<<sum;
+    sort ( a+1 ,a+ 3);
+}
+int main() {
+    nhapso();
+    cout << a<<b<<c;
+    sapxep();
     return 0;
 }
