ae0a6428bf8342b04f6980af4023b6b31d47573a
Mint Bao <minhbao1223@gmail.com>
2021-11-27 15:46:32 +0700

    11111

--- bai1vd.cpp
+++ bai1vd.cpp
@@ -2,18 +2,21 @@
 #include<iostream>
 
 using namespace std;
-
-int main() {
-    int a[3];
+void nhapso(int &a ,int &b, int &c){
     cout << "Nhap lan luot 3 so: ";
-    for (int i = 0; i < 3; i++) {
-        cin >> a[i];
+    cin >> a>>b>>c;
+}
+void sapxep(){
+    int a , b, c;
+    nhapso(a,b,c);
+    int arr[3]= {a,b,c};
+    sort (arr , arr +3);
+    cout << "3 so tang dan la: ";
+	for (int i=0 ; i<3 ; i++){
+        cout << arr[i] << " ";
     }
-    sort ( a , a+3);
-    cout << "3 so thu tu tang dan la: ";
-    for (int i=0 ; i<3 ; i++){
-        cout << a[i] <<" ";
-    }
-
+}
+int main() {
+    sapxep();
     return 0;
 }
