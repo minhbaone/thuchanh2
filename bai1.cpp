9e93a09c90df22ae5525c67d6218d0706fb6b1b7
Mint Bao <minhbao1223@gmail.com>
2021-11-27 13:11:19 +0700

    haha

--- bai1.cpp
+++ bai1.cpp
@@ -3,21 +3,14 @@
 
 using namespace std;
 
-int nhapso(){
+int main() {
     int a,b,c;
     cout << "Nhap lan luot  3 so: ";
     cin >> a>>b>> c;
-}
-int sapxep(){
     int a[10];
     for (int i=1 ; i<=3 ; i++){
         cin >> a[i];
     }
     sort ( a+1 ,a+ 3);
-}
-int main() {
-    nhapso();
-    cout << a<<b<<c;
-    sapxep();
     return 0;
 }
