95afd85036169ce2aad03b2edf6417f0e2803c59
Mint Bao <minhbao1223@gmail.com>
2021-11-27 16:46:04 +0700

    ahaha

---
+++ bai2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    float a,b,c,delta,x1,x2;
+    // Phuong trinh ax^2+bs+c=0
+    cout<< "Nhap lan luot a,b,c: ";
+    cin >> a >> b >> c ;
+    delta = b*b - 4*a*c;
+    if(a==0){
+        cout << "Nghiem pt la: " << -c /b;
+    }
+    if( a!= 0 ){
+        if (delta < 0) {
+            cout << "Phuong trinh vo nghiem: ";
+        } else if (delta ==0 ){
+            cout << "Phuong trinh co nghiem kep x= " << -b / 2*a;
+        } else {
+            x1 = (-b + sqrt(delta))/2*a;
+            x1 = (-b - sqrt(delta))/2*a;
+            cout << "Phuong trinh co 2 nghiem x1= " << x1 << " va x2 = "<<x2;
+        }
+    }
+    return 0;
+}
